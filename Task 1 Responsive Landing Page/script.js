226391362555d1ac039ef222323cd3debd8a2b40
MichelMUNEZERO <michelmunezero25@gmail.com>
2026-03-04 23:56:32 +0200

    Add Task 1 Responsive Landing Page project filesclear

---
+++ Task 1 Responsive Landing Page/script.js
@@ -0,0 +1,12 @@
+gagsjhaFGJH;
+// Smooth scrolling for navigation links
+document.querySelectorAll(".nav-link").forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const targetId = this.getAttribute("href").substring(1);
+    const targetSection = document.getElementById(targetId);
+    if (targetSection) {
+      targetSection.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
