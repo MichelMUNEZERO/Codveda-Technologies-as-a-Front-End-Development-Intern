efa69bfbb248f78a9a60d7a8a7fb2798ba867a0c
MichelMUNEZERO <michelmunezero25@gmail.com>
2026-03-10 07:34:10 +0200

    Fix some issues about css

--- Task 3 Basic DOM Manipulation/script.js
+++ Task 3 Basic DOM Manipulation/script.js
@@ -1,13 +1,18 @@
 const incrementBtn = document.querySelector(".increment-btn");
 const decrementBtn = document.querySelector(".decrement-btn");
 const counter = document.querySelector(".counter");
+
 let count = 0;
 
-incrementBtn.addEventListener("click", () => {
-  count++;
-  counter.textContent = count;
-});
-decrementBtn.addEventListener("click", () => {
-  count--;
-  counter.textContent = count;
-});
+// Check if elements exist
+if (incrementBtn && decrementBtn && counter) {
+  incrementBtn.addEventListener("click", () => {
+    count++;
+    counter.textContent = count;
+  });
+
+  decrementBtn.addEventListener("click", () => {
+    count--;
+    counter.textContent = count;
+  });
+}
