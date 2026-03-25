f5886d172baecde48347858fbae287793777b876
MichelMUNEZERO <michelmunezero25@gmail.com>
2026-03-25 16:29:34 +0200

    Correct the word signin

--- Level 3 Advanced/Task 1 Custom Component Library/backend/app/urls.py
+++ Level 3 Advanced/Task 1 Custom Component Library/backend/app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('siginup/', views.siginup, name='siginup'),
-    path('signin/', views.signin, name='signin'),
+    path('sign/', views.signin, name='signin'),
     path('forgot-password/', views.forgot_password, name='forgot_password'),
 ]
